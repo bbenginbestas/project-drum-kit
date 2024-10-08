df38037d12f979c19f79cb8bccf611a5fa9f0751
bbenginbestas <bestasbengin@gmail.com>
2024-10-08 15:22:12 +0300

    Add drum sounds to buttons

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        var audio = new Audio("./sounds/tom-1.mp3");
-        audio.play();
+        if (i<4) {
+            var audio = new Audio("./sounds/tom-" + (i+1) + ".mp3");
+            audio.play();
+        } else if (i==4) {
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+        } else if (i==5) {
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+        } else if (i==6) {
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+        }
     });
 }

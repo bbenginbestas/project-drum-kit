76782b5e5937142c7167742ee236c4488fe7c9e4
bbenginbestas <bestasbengin@gmail.com>
2024-10-08 15:17:40 +0300

    Add drum images to buttons

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("Click")
+        var audio = new Audio("./sounds/tom-1.mp3");
+        audio.play();
     });
 }
